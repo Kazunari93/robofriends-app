d011c7a82158f72ee0d41cfcb953ff8167a3ebf9
Kazunari93 <aooni0930@gmail.com>
2020-03-14 23:26:05 +0900

    styling

--- robofriends/src/components/Card.jsx
+++ robofriends/src/components/Card.jsx
@@ -3,7 +3,12 @@ import React from "react";
 const Card = ({ id, name, email }) => {
   return (
     <div className="bg-washed-red dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img alt="robots" src={`https://robohash.org/${id}?200×200`} />
+      <img
+        alt="robots"
+        src={`https://robohash.org/${id}`}
+        width="100"
+        height="100"
+      />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
